0b28e6f69d8c92deedcc343254d5a3838bc684fd
ai208 <yasu6328hand@gmail.com>
2026-03-14 12:28:46 +0900

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,1 @@
-import express from "express";
-import cors from "cors";
-import authRoutes from "./routes/authRoutes.js";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
