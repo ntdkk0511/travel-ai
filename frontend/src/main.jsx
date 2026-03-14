f7d56d59366577165dafb099c2c49d7ece764cb5
ai208 <yasu6328hand@gmail.com>
2026-03-14 20:25:57 +0900

    app に変更

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import Test from './test.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Test/>
+    <App />
+    {/* <Test/> */}
   </StrictMode>,
 )
