6bbc9729c1ad8e965d392fd54952b52c285a3a64
ai208 <yasu6328hand@gmail.com>
2026-03-14 12:50:42 +0900

    login_model

---
+++ backend/models/UserModel.js
@@ -0,0 +1,59 @@
+// データ構造とDB操作をまとめる場所
+// 最初は　userから作る　plan と weight はuserIdで紐づくから
+
+// uuidがいる
+const {v4 :uuidv4} = require("uuid");
+
+// getalluserの準備　ファイルの読み取り
+const fs = require("fs") //ファイルの読み取り
+const path = require("path") //
+
+
+const dataFile = path.join(__dirname,"../data/users.json"); //__dirname はこのファイルの場所 ..は一つ上
+
+
+class UserModel{ // フォイルを指定する 性別もあった方がいい 年齢と身長も追加しないといけない。
+    constructor(id,name,email,password){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    // なくても呼べる jsonの読み込み json → 配列
+    static getAllUsers(file = dataFile){
+        try{
+            const data = fs.readFileSync(file,"utf-8");//読み取り
+            return JSON.parse(data || "[]"); // json → js の配列に からのときも
+        }catch(err){
+            if(err.code==="ENOENT"){
+                return []; //ない時はから配列
+            }
+            throw err;//エラーが表示される
+        }
+    }
+    // CRUD を作る。
+    // getAlluserの逆をする　配列　→ json 対象を示す
+    save(file = dataFile){
+        const users = UserModel.getAllUsers(file);
+        users.push(this);
+        fs.writeFileSync(file,JSON.stringify(users,null,2));
+        return this;
+    }
+    // モデルを作らないくても　呼び出せる static
+    static createUser(name,email,password,file = dataFile){
+        const id = uuidv4();
+        const user = new UserModel(id,name,email,file,password);
+        user.save(file);
+        return user;
+    }
+    // 対象になるファイルを明言する
+    static getUserById(id,file = dataFile){
+        users = UserModel.getAllUsers(file);
+        return users.find(user => user.id === id); //id と同じuserを{}で取ってくる
+    }
+
+}
+module.exports = UserModel; //エキスポートする
+
---
+++ backend/tests/UserModel.test.js
@@ -0,0 +1,41 @@
+// test jestを使っている。
+
+const fs = require("fs");
+const path = require("path");
+
+const UserModel = require("../models/UserModel");
+const testDataFile = path.join(__dirname,"../data/users.test.json");
+
+// testように変更する
+// UserModel.dataFile = testDataFile;
+
+beforeEach(()=>{
+    // から配列で初期化する
+    fs.writeFileSync(testDataFile,"[]","utf-8");
+});
+
+afterAll(()=>{
+    //テスト後にファイルを削除
+    fs.unlinkSync(testDataFile);
+});
+// 名前と行う関数
+// 作成とjson
+test("createUser should return a user with id,name,email",()=>{
+    const user = UserModel.createUser("alice","ai.gmail.com","1234",file = testDataFile);
+    //うまく作れているのかの確認
+    expect(user).toHaveProperty("id");
+    expect(user.name).toBe("alice");
+    expect(user.email).toBe("ai.gmail.com");
+    // jsonファイルの確認
+    const usersInfile = JSON.parse(fs.readFileSync(testDataFile,"utf-8"));
+    expect(usersInfile.length).toBe(1);
+    expect(usersInfile[0].name).toBe("alice");
+
+});
+// by idのテスト
+test("get_user_by_id",()=>{
+    const user = UserModel.createUser("ai","ai.gmail.com","1234",file=testDataFile);
+    const found = UserModel.getUserById(user.id,testDataFile);
+    expect(user.name).toBe(found.name);
+
+})
